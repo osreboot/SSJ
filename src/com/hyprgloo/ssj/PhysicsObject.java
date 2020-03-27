75a8ace5230653c271d087df5137eb246cf2d35f
osreboot <calvin.16.weaver@gmail.com>
2020-03-27 15:08:50 -0700

    PhysicsObject and Player

---
+++ src/com/hyprgloo/ssj/PhysicsObject.java
@@ -0,0 +1,60 @@
+package com.hyprgloo.ssj;
+
+import com.osreboot.ridhvl.HvlCoord2D;
+import com.osreboot.ridhvl.HvlMath;
+
+public class PhysicsObject {
+	
+	public HvlCoord2D speed;
+	
+	private HvlCoord2D location;
+	private float angle, radius;
+	
+	private PhysicsObject parent;
+	private float parentDistance, parentAngle;
+	
+	public PhysicsObject(float radiusArg, float xArg, float yArg, float angleArg){
+		radius = radiusArg;
+		location = new HvlCoord2D(xArg, yArg);
+		speed = new HvlCoord2D();
+		angle = angleArg;
+	}
+	
+	public void connectToParent(PhysicsObject parentArg){
+		parent = parentArg;
+		parentDistance = HvlMath.distance(parent.getPosition(), getPosition());
+		parentAngle = HvlMath.fullRadians(parent.getPosition(), getPosition());
+	}
+	
+	public void disconnectFromParent(){
+		parent = null;
+	}
+	
+	public boolean hasParent(){
+		return parent != null;
+	}
+	
+	public HvlCoord2D getPosition(){
+		if(hasParent()){
+			float x = parent.getPosition().x + ((float)Math.cos(Math.toRadians(parent.getAngle() + parentAngle)) * parentDistance);
+			float y = parent.getPosition().y + ((float)Math.sin(Math.toRadians(parent.getAngle() + parentAngle)) * parentDistance);
+			location = new HvlCoord2D(x, y);
+			return location;
+		}else return location;
+	}
+	
+	public float getAngle(){
+		if(hasParent()){
+			return parent.getAngle() + angle;
+		}else return angle;
+	}
+	
+	public void update(float delta){
+		location.add(speed.multNew(delta));
+	}
+	
+	public float getRadius(){
+		return radius;
+	}
+	
+}
---
+++ src/com/hyprgloo/ssj/Player.java
@@ -0,0 +1,30 @@
+package com.hyprgloo.ssj;
+
+import static com.osreboot.ridhvl.painter.painter2d.HvlPainter2D.hvlDrawQuadc;
+
+import org.lwjgl.input.Keyboard;
+import org.newdawn.slick.Color;
+
+public class Player {
+
+	public static final float ACCELERATION = 100f;
+	
+	public PhysicsObject physicsObject;
+	
+	public Player(){
+		physicsObject = new PhysicsObject(0f, 0f, 0f, 100f);
+	}
+	
+	public void update(float delta){
+		float xsInput = (Keyboard.isKeyDown(Keyboard.KEY_A) ? -ACCELERATION : 0) + (Keyboard.isKeyDown(Keyboard.KEY_D) ? ACCELERATION : 0);
+		float ysInput = (Keyboard.isKeyDown(Keyboard.KEY_W) ? -ACCELERATION : 0) + (Keyboard.isKeyDown(Keyboard.KEY_S) ? ACCELERATION : 0);
+		physicsObject.speed.add(xsInput * delta, ysInput * delta);
+		
+		physicsObject.update(delta);
+	}
+	
+	public void draw(float delta){
+		hvlDrawQuadc(physicsObject.getPosition().x, physicsObject.getPosition().y, physicsObject.getRadius() * 2f, physicsObject.getRadius() * 2f, Color.blue);
+	}
+	
+}
