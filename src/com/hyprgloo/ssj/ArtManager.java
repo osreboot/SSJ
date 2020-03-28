dcb91678b8be97bf223e19b50c72428f901701ae
osreboot <calvin.16.weaver@gmail.com>
2020-03-28 00:11:40 -0700

    drfsdfasdf

--- src/com/hyprgloo/ssj/ArtManager.java
+++ src/com/hyprgloo/ssj/ArtManager.java
@@ -3,15 +3,22 @@ package com.hyprgloo.ssj;
 import static com.osreboot.ridhvl.painter.painter2d.HvlPainter2D.hvlDrawQuad;
 
 import org.lwjgl.opengl.Display;
+import org.newdawn.slick.Color;
 
 public class ArtManager {
 
+	public static final float
+	SCALE_PLASMA = 0.0001f;
+	
+	public static final Color
+	COLOR_PLASMA = new Color(0.4f, 0.4f, 0.4f);
+	
 	public static void drawBackground(float xArg, float yArg){
-		float uOffset = xArg * 0.001f;
-		float vOffset = yArg * 0.001f;
+		float uOffset = xArg * SCALE_PLASMA;
+		float vOffset = yArg * SCALE_PLASMA;
 		hvlDrawQuad(0, 0, Display.getWidth(), Display.getHeight(),
 				uOffset, vOffset, uOffset + (Display.getWidth() / 2048f), vOffset + (Display.getHeight() / 2048f),
-				Main.getTexture(Main.INDEX_PLASMA));
+				Main.getTexture(Main.INDEX_PLASMA), COLOR_PLASMA);
 	}
 	
 }
