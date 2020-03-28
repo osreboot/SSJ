8e559f4563c2ab628681fab1766d90138cd7c935
Zack <dukeandjasper@gmail.com>
2020-03-27 22:26:47 -0500

    asteroid time 3

--- src/com/hyprgloo/ssj/Asteroid.java
+++ src/com/hyprgloo/ssj/Asteroid.java
@@ -4,35 +4,52 @@ import static com.osreboot.ridhvl.painter.painter2d.HvlPainter2D.hvlDrawQuadc;
 import static com.osreboot.ridhvl.painter.painter2d.HvlPainter2D.hvlResetRotation;
 import static com.osreboot.ridhvl.painter.painter2d.HvlPainter2D.hvlRotate;
 
-import org.newdawn.slick.Color;
-
 import com.osreboot.ridhvl.HvlCoord2D;
+import com.osreboot.ridhvl.HvlMath;
 
 public class Asteroid {
-
+	
 	private float rotationSpeed;
 	public PhysicsObject physicsObject;
 	
-	private boolean typeAssigned;
+	public int asteroidType;
+	private int typeHandler;
+	private boolean typeAssigned = false;
 
-	public Asteroid(float xArg, float yArg, float xSpeedArg, float ySpeedArg, float rotationArg, float rotationSpeedArg, float sizeArg) {
+	public Asteroid(HvlCoord2D pos, boolean jrArg) {
+		
+		float rotationArg = HvlMath.randomFloatBetween(0, 3.14f);
+		float rotationSpeedArg = HvlMath.randomFloatBetween(-200, 200);
+		float sizeArg = (jrArg)?HvlMath.randomFloatBetween(50, 500):HvlMath.randomFloatBetween(50, 500);
 		
 		rotationSpeed = rotationSpeedArg;
 		
-		physicsObject = new PhysicsObject(xArg, yArg, rotationArg, sizeArg);
+		physicsObject = new PhysicsObject(pos.x, pos.y, rotationArg, sizeArg);
 		 
 	}
 	
 	public void assignType() {
 		if(!typeAssigned) {
 			
-			
-			
+			typeHandler = HvlMath.randomIntBetween(0, 100);
+			if(typeHandler > 20) {
+				asteroidType = 1;
+			}else {
+				asteroidType = 2;
+			}
+			typeAssigned = true;
 		}
 	}
 	
 	public void update(float delta) {
+		
+		if(asteroidType == 1) {
+			Asteroid jr = new Asteroid(new HvlCoord2D(physicsObject.location.x, physicsObject.location.y), true);
+			jr.physicsObject.connectToParent(physicsObject);
+		}
+		
 		physicsObject.update(delta);
+		
 	}
 	
 	public void draw() {
