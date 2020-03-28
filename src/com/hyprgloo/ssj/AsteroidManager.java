67cda5998c2e6b00c5b8668391c67ea59cabf0d8
haveaniceday33 <munrosamuel@gmail.com>
2020-03-28 00:31:31 -0400

    min asteroid spawn

--- src/com/hyprgloo/ssj/AsteroidManager.java
+++ src/com/hyprgloo/ssj/AsteroidManager.java
@@ -1,33 +1,39 @@
 package com.hyprgloo.ssj;
+
 import java.util.ArrayList;
 
 import com.osreboot.ridhvl.HvlCoord2D;
 import com.osreboot.ridhvl.HvlMath;
 
 public class AsteroidManager {
-	
+
 	private static ArrayList<Asteroid> asteroids;
-	
+
 	public static void initAsteroids() {
 		asteroids = new ArrayList<>();
-	
-		for(int i = 0; i < 20000; i++) {
-			
+
+		for (int i = 0; i < 20000; i++) {
+
 			HvlCoord2D asPos = new HvlCoord2D();
-			
+
 			asPos.x = HvlMath.randomFloatBetween(-50000, 50000);
 			asPos.y = HvlMath.randomFloatBetween(-50000, 50000);
-			
+
+			while (HvlMath.distance(asPos.x, asPos.y, 0, 0) < 750) {
+				asPos.x = HvlMath.randomFloatBetween(-50000, 50000);
+				asPos.y = HvlMath.randomFloatBetween(-50000, 50000);
+			}
+
 			Asteroid a = new Asteroid(asPos, false);
 			asteroids.add(a);
 		}
-		
+
 	}
-	
+
 	public static void update(float delta) {
-		for(Asteroid a : asteroids) {
+		for (Asteroid a : asteroids) {
 			a.assignType();
-			if(HvlMath.distance(Game.player.physicsObject.location, a.physicsObject.location) < 1200) {
+			if (HvlMath.distance(Game.player.physicsObject.location, a.physicsObject.location) < 1200) {
 				a.update(delta);
 				a.draw();
 				a.physicsObject.canDealDamage = true;
