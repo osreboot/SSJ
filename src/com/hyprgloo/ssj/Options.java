ae997875d7ec7c5ea1765f98f61483f9f517a64d
haveaniceday33 <munrosamuel@gmail.com>
2020-03-29 15:47:53 -0400

    Extra music option

--- src/com/hyprgloo/ssj/Options.java
+++ src/com/hyprgloo/ssj/Options.java
@@ -2,11 +2,12 @@ package com.hyprgloo.ssj;
 
 public class Options {
 
-	public static boolean sound, tutorials;
+	public static boolean sound, tutorials, music;
 	
 	public static void init(){
 		sound = true;
 		tutorials = true;
+		music = true;
 	}
 	
 }
